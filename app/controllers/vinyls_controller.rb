3dc1c4221ef58d7290b2ffe4260fc7bb90d8454f
risatino <charissatolentino@gmail.com>
2016-08-29 20:40:55 -0500

    added information for 4 vinyl records into my Model table

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,2 +1,8 @@
 class VinylsController < ApplicationController
+  def all_artists
+    @artists = Vinyl.find
+  end
+
+  def artist
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Vinyl.create(artist_name: 'Phish', record_title: 'Fuego', genre: 'Rock', description: 'Tubular', playtime: 45, label: 'JEMP', price: 17)
+Vinyl.create(artist_name: 'Nirvana', record_title: 'Nevermind', genre: 'Rock', description: 'Grunge Rock', playtime: 35, label: 'DGC', price: 21)
+Vinyl.create(artist_name: 'Radiohead', record_title: 'Ok Computer', genre: 'Alternative Rock', description: 'Awesome, chill, yup.', playtime: 40, label: 'Parlophone', price: 25)
+Vinyl.create(artist_name: 'John Carpenter', record_title: 'Lost Themes II', genre: 'Sountracks, Electronic, Ghost Music', description: 'Haunted and sweet.', playtime: 42, label: 'Sacred Bones', price: 20)
+
