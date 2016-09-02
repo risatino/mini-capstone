18d866bb9eb05edc94748f2f2877ed104ed4484b
risatino <charissatolentino@gmail.com>
2016-09-01 21:24:09 -0500

    updates to mini-capstone

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,9 +1,57 @@
 class VinylsController < ApplicationController
-  def all_artists
-    @artists = Vinyl.all
+  def index
+    @vinyls = Vinyl.all
   end
 
-  def artist
-    @artist = Vinyl.all.sample
+  def new
+  
+  end
+
+  def create
+    @vinyl = Vinyl.create(artist_name: params[:artist_name], 
+                          record_title: params[:record_title],
+                          genre: params[:genre],
+                          description: params[:description],
+                          playtime: params[:playtime],
+                          label: params[:label],
+                          price: params[:price])
+
+    render 'show.html.erb'
+  end
+
+  def show
+    @vinyl = Vinyl.find(params[:id])
+  end
+
+  def edit
+    @vinyl = Vinyl.find(params[:id])      
+  end
+
+  def update
+    @vinyl = Vinyl.find(params[:id])
+    @vinyl.update(artist_name: params[:artist_name], 
+                  record_title: params[:record_title],
+                  genre: params[:genre],
+                  description: params[:description],
+                  playtime: params[:playtime],
+                  label: params[:label],
+                  price: params[:price])
+    
+  # @vinyl.assign_attributes(artist_name: params[:artist_name], 
+  #                     record_title: params[:record_title],
+  #                     genre: params[:genre],
+  #                     description: params[:description],
+  #                     playtime: params[:playtime],
+  #                     label: params[:label],
+  #                     price: params[:price])
+  # @recipe.save
   end
 end
+
+  # def all_artists
+  #   @artists = Vinyl.all
+  # end
+
+  # def artist
+  #   @artist = Vinyl.all.sample
+  # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  get "/" => "vinyls#all_artists"
-  get "/artist" => "vinyls#artist"
-end
+  get '/' => 'vinyls#index'
+  get '/vinyls' => 'vinyls#index'
+
+  get '/vinyls/new' => 'vinyls#new'
+  post '/vinyls' => 'vinyls#create'
+
+  get '/vinyls/:id' => 'vinyls#show'
+
+  get '/vinyls/:id/edit' => 'vinyls#edit'
+  patch '/vinyls/:id' => 'vinyls#update'
+end  
+  # get '/vinyls/new' => 'vinyls#new'
+  # get '/' => 'vinyls#all_artists'
+  # get '/artist' => 'vinyls#artist'
+
