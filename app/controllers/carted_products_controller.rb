3dfc69d3dad758115464e4b8293712cd0c893570
risatino <charissatolentino@gmail.com>
2016-09-19 20:56:23 -0500

    updates but having trouble getting my carted products view

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,9 +1,14 @@
 class CartedProductsController < ApplicationController
+  
+  def index
+    @user_id = current_user.id
+    @carted_products = CartedProduct.where(status: "Carted")
+  end
 
   def create
     @vinyl = Vinyl.find(params[:vinyl_id])
 
-    @carted_product = CartedProduct.new(user_id: current_user.id, vinyl_id: params[:vinyl_id], quantity: params[:quantity].to_i, :status "carted")
+    @carted_product = CartedProduct.new(user_id: params[:user_id], vinyl_id: params[:vinyl_id], quantity: params[:quantity], status: "carted")
 
     @carted_product.save
 
@@ -11,9 +16,4 @@ class CartedProductsController < ApplicationController
     redirect_to "/carted_products"
   end
 
-  def index
-
-    @user_id = current_user.id
-    @carted_products = CartedProduct.where(user_id: current_user.id AND status: "carted")
-  end
 end
