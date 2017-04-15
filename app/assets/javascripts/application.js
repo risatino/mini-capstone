3967d2d0349620d81794c846c273d96d88b1bea1
risatino <charissatolentino@gmail.com>
2017-04-14 22:06:02 -0500

    modified front-end and started about page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,15 +15,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-var heartuptimer = setInterval(heartup, 200);
-function heartup() {
-  $(".heart").css("font-weight", "900");
-}
-
-var heartdowntimer = setInterval(heartdown, 500);
-function heartdown() {
-  $(".heart").css("font-weight", "100");
-}
-
-// $('.carousel').carousel();
